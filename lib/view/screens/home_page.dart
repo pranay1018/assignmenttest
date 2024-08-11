c3e0c71f80ea9311482e7e638ceb8d9d58f67573
pranay <pranay.pathrabe@pragmatyc.com>
2024-08-11 23:55:25 +0530

    progress indicator implemented at homepage

--- lib/view/screens/home_page.dart
+++ lib/view/screens/home_page.dart
@@ -2,6 +2,8 @@ import 'package:assignmenttest/utils/constants/colors.dart';
 import 'package:assignmenttest/utils/constants/sizes.dart';
 import 'package:assignmenttest/utils/constants/text_strings.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../providers/providers.dart';
 import '../widgets/category_widget.dart';
 import '../widgets/custom_appbar.dart';
 import '../widgets/recently_ordered_widget.dart';
@@ -10,11 +12,14 @@ import '../widgets/miniCartNudge.dart';
 import '../widgets/search_bar.dart';
 import '../widgets/vertical_product_list.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final overallState = ref.watch(
+        homeViewModelProvider.select((state) => state.overallState));
+
     return Scaffold(
       appBar: CustomAppBar(
         title: MyTexts.appName,
@@ -22,27 +27,35 @@ class HomePage extends StatelessWidget {
           debugPrint('Logout pressed');
         },
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          color: MyColors.primary.withOpacity(0.2),
-          padding: const EdgeInsets.all(MySizes.md),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              searchBarWidget(),
-              const SizedBox(height: MySizes.md),
-              const ImageBannerList(),
-              const SizedBox(height: MySizes.md),
-              const CategoryWidget(),
-              const SizedBox(height: MySizes.md),
-              const RecentlyOrderedWidget(),
-              const SizedBox(height: MySizes.md),
-              const ProductList(), // Remove Expanded here
-            ],
-          ),
-        ),
+      body: overallState.when(
+        data: (data) => _buildHomeWidget(),
+        error: (error, stackTrace) => Center(child: Text("Error: $error")),
+        loading: () => const Center(child: CircularProgressIndicator()),
       ),
       bottomNavigationBar: const MiniCartNudge(),
+    );
+  }
+
+  Widget _buildHomeWidget() {
+    return SingleChildScrollView(
+      child: Container(
+        color: MyColors.primary.withOpacity(0.2),
+        padding: const EdgeInsets.all(MySizes.md),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            searchBarWidget(),
+            const SizedBox(height: MySizes.md),
+            const ImageBannerList(),
+            const SizedBox(height: MySizes.md),
+            const CategoryWidget(),
+            const SizedBox(height: MySizes.md),
+            const RecentlyOrderedWidget(),
+            const SizedBox(height: MySizes.md),
+            const ProductList(), // Ensure this list is scrollable if needed
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/view_model/home_view_model.dart
+++ lib/view_model/home_view_model.dart
@@ -1,66 +1,57 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import '../models/models.dart';
-import '../repository/home_repository.dart'; // Import your VerticalProductModel
+import '../repository/home_repository.dart';
 
 class HomeViewModel extends StateNotifier<HomeState> {
   HomeViewModel(this._repository) : super(HomeState.initial()) {
-    _fetchBanners();
-    _fetchCategories();
-    _fetchRecentlyOrderedProducts();
-    _fetchProducts();
+    _fetchAllData();
   }
 
   final HomeRepository _repository;
 
-  Future<void> _fetchBanners() async {
-    state = state.copyWith(banners: const AsyncValue.loading());
-    try {
-      final banners = await _repository.fetchBanners();
-      state = state.copyWith(banners: AsyncValue.data(banners));
-    } catch (e) {
-      state = state.copyWith(categories: AsyncValue.error(e,StackTrace.current));
-    }
-  }
+  Future<void> _fetchAllData() async {
+    state = state.copyWith(overallState: const AsyncValue.loading());
 
-  Future<void> _fetchCategories() async {
-    state = state.copyWith(categories: const AsyncValue.loading());
     try {
-      final categories = await _repository.fetchCategories();
-      state = state.copyWith(categories: AsyncValue.data(categories));
-    } catch (e) {
-      state = state.copyWith(categories: AsyncValue.error(e,StackTrace.current));
-    }
-  }
+      final bannersFuture = _repository.fetchBanners();
+      final categoriesFuture = _repository.fetchCategories();
+      final horizontalProductsFuture = _repository.fetchRecentlyOrderedProducts();
+      final verticalProductsFuture = _repository.fetchProducts();
 
-  Future<void> _fetchRecentlyOrderedProducts() async {
-    state = state.copyWith(horizontalProducts: const AsyncValue.loading());
-    try {
-      final products = await _repository.fetchRecentlyOrderedProducts();
-      state = state.copyWith(horizontalProducts: AsyncValue.data(products));
-    } catch (e) {
-      state = state.copyWith(categories: AsyncValue.error(e,StackTrace.current));
-    }
-  }
+      final banners = await bannersFuture;
+      final categories = await categoriesFuture;
+      final horizontalProducts = await horizontalProductsFuture;
+      final verticalProducts = await verticalProductsFuture;
 
-  Future<void> _fetchProducts() async {
-    state = state.copyWith(verticalProducts: const AsyncValue.loading());
-    try {
-      final products = await _repository.fetchProducts();
-      state = state.copyWith(verticalProducts: AsyncValue.data(products));
+      state = state.copyWith(
+        overallState: AsyncValue.data(null), // Data is loaded successfully
+        banners: AsyncValue.data(banners),
+        categories: AsyncValue.data(categories),
+        horizontalProducts: AsyncValue.data(horizontalProducts),
+        verticalProducts: AsyncValue.data(verticalProducts),
+      );
     } catch (e) {
-      state = state.copyWith(categories: AsyncValue.error(e,StackTrace.current));
+      state = state.copyWith(
+        overallState: AsyncValue.error(e,StackTrace.current),
+        banners: AsyncValue.error(e,StackTrace.current),
+        categories: AsyncValue.error(e,StackTrace.current),
+        horizontalProducts: AsyncValue.error(e,StackTrace.current),
+        verticalProducts: AsyncValue.error(e,StackTrace.current),
+      );
     }
   }
 }
 
+
 class HomeState {
+  final AsyncValue<void> overallState; // New unified state
   final AsyncValue<List<BannerModel>> banners;
   final AsyncValue<List<CategoryModel>> categories;
   final AsyncValue<List<ProductModel>> horizontalProducts;
   final AsyncValue<List<ProductModel>> verticalProducts;
 
   HomeState({
+    required this.overallState,
     required this.banners,
     required this.categories,
     required this.horizontalProducts,
@@ -69,6 +60,7 @@ class HomeState {
 
   factory HomeState.initial() {
     return HomeState(
+      overallState: const AsyncValue.loading(),
       banners: const AsyncValue.loading(),
       categories: const AsyncValue.loading(),
       horizontalProducts: const AsyncValue.loading(),
@@ -77,12 +69,14 @@ class HomeState {
   }
 
   HomeState copyWith({
+    AsyncValue<void>? overallState,
     AsyncValue<List<BannerModel>>? banners,
     AsyncValue<List<CategoryModel>>? categories,
     AsyncValue<List<ProductModel>>? horizontalProducts,
     AsyncValue<List<ProductModel>>? verticalProducts,
   }) {
     return HomeState(
+      overallState: overallState ?? this.overallState,
       banners: banners ?? this.banners,
       categories: categories ?? this.categories,
       horizontalProducts: horizontalProducts ?? this.horizontalProducts,
